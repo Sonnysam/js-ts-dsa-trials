6a1572568594f274c0c72bf6ed18a98bc56f662a
sonnysam <samuelagbenyo067@gmail.com>
2024-03-27 07:48:57 +0000

    string reversal added

--- string-reversal/index.js
+++ string-reversal/index.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+function reverseWord(str) {
+  let reversedString = "";
+
+  for (let char of str) {
+    reversedString = char + reversedString;
+  }
+
+  return reversedString;
+}
+
+console.log(reverseWord("Hello"));
--- string-reversal/index.ts
+++ string-reversal/index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+function ReverseWord(str: string): string {
+  let reversedString = "";
+
+  for (let i = 0; i < str.length; i++) {
+    reversedString = str[i] + reversedString;
+  }
+
+  return reversedString;
+}
+
+console.log(ReverseWord("Hello"));
